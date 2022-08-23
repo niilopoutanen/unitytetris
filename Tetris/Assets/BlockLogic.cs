867c1824b84ca99265216507dbce75ccb4e46c37
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-08-23 13:44:48 +0300

    removed savetest

--- Tetris/Assets/BlockLogic.cs
+++ Tetris/Assets/BlockLogic.cs
@@ -67,7 +67,6 @@ public class BlockLogic : MonoBehaviour
     void Start()
     {
         FindObjectOfType<UIClass>().PauseMenu(false);
-        //FindObjectOfType<SaveSystem>().SaveScores(4, "testi");
 
         if (!ValidPos())
         {
