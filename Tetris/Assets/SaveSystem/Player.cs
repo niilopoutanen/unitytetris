7a9731fd755f6f7c4ca73c3bff33f9a4aa72a2eb
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-09-01 20:03:49 +0300

    working save system, not yet connected to game

--- Tetris/Assets/SaveSystem/Player.cs
+++ Tetris/Assets/SaveSystem/Player.cs
@@ -6,4 +6,19 @@ public class Player : MonoBehaviour
 {
     public int score;
     public int blocksPlaced;
+
+    public void SavePlayer()
+    {
+        SaveSystem.SaveData(this);
+        Debug.Log("Saved");
+    }
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadData();
+
+        score = data.highScore;
+        blocksPlaced = data.blocksPlaced;
+        Debug.Log("Loaded");
+    }
+
 }
