df92fe6799bf77ed5c7b6a7c5390be966094b18c
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-09-02 15:21:54 +0300

    modified save system to json

--- Tetris/Assets/SaveSystem/SaveSystem.cs
+++ Tetris/Assets/SaveSystem/SaveSystem.cs
@@ -4,34 +4,55 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public static class SaveSystem
 {
+    //public static void SaveData(Player player)
+    //{
+    //    BinaryFormatter formatter = new BinaryFormatter();
+    //    string path = Application.persistentDataPath + "/save.npgame";
+    //    FileStream stream = new FileStream(path, FileMode.Create);
+
+    //    PlayerData data = new PlayerData(player);
+
+    //    formatter.Serialize(stream, data);
+    //    stream.Close();
+    //}
+
+    //public static PlayerData LoadData()
+    //{
+    //    string path = Application.persistentDataPath + "/save.npgame";
+    //    if (File.Exists(path))
+    //    {
+    //        BinaryFormatter binaryreader = new BinaryFormatter();
+    //        FileStream stream = new FileStream(path, FileMode.Open);
+
+    //        PlayerData data = binaryreader.Deserialize(stream) as PlayerData;
+    //        stream.Close();
+    //        return data;
+    //    }
+    //    else
+    //    {
+    //        Debug.LogError("No save file in" + path);
+    //        return null;
+    //    }
+    //}
+
     public static void SaveData(Player player)
     {
-        BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + "/save.npgame";
-        FileStream stream = new FileStream(path, FileMode.Create);
-
-        PlayerData data = new PlayerData(player);
-
-        formatter.Serialize(stream, data);
-        stream.Close();
+        string path = Application.persistentDataPath + "/save.json";
+        string json = JsonUtility.ToJson(player);
+        using (StreamWriter writer = new StreamWriter(path))
+        {
+            writer.Write(json);
+        }
     }
-
     public static PlayerData LoadData()
     {
-        string path = Application.persistentDataPath + "/save.npgame";
-        if (File.Exists(path))
-        {
-            BinaryFormatter binaryreader = new BinaryFormatter();
-            FileStream stream = new FileStream(path, FileMode.Open);
+        string path = Application.persistentDataPath + "/save.json";
 
-            PlayerData data = binaryreader.Deserialize(stream) as PlayerData;
-            stream.Close();
-            return data;
-        }
-        else
+        using (StreamReader reader = new StreamReader(path))
         {
-            Debug.LogError("No save file in" + path);
-            return null;
+            string json = reader.ReadToEnd();
+            PlayerData returnData = JsonUtility.FromJson<PlayerData>(json);
+            return returnData;
         }
     }
 }
