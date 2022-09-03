409fb1ee4c0eb4a5b702dfdb752fec132197178e
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-09-03 12:09:18 +0300

    working first achievement

--- Tetris/Assets/AchievementMenu/Achievement.cs
+++ Tetris/Assets/AchievementMenu/Achievement.cs
@@ -7,8 +7,8 @@ using UnityEngine.UIElements;
 
 public class Achievement : MonoBehaviour
 {
-    public int totalBlocks = 500;
-    public int ScoreOver20times = 10;
+    private int totalBlocks = 500;
+    private int ScoreOver20times = 10;
 
     public bool CheckIfDone(int requirement, int value)
     {
@@ -21,8 +21,22 @@ public class Achievement : MonoBehaviour
             return false;
         }
     }
+    public void ChangeVisibility(bool isSilver,string componentName)
+    {
+        GameObject tochange = GameObject.Find(componentName);
 
-
+        if (isSilver == true)
+        {
+            tochange.transform.Find("Silver").gameObject.SetActive(true);
+            tochange.transform.Find("Locked").gameObject.SetActive(false);
+        }
+        if(isSilver == false)
+        {
+            tochange.transform.Find("Gold").gameObject.SetActive(true);
+            tochange.transform.Find("Locked").gameObject.SetActive(false);
+        }
+        tochange.transform.Find("Icon").gameObject.SetActive(true);
+    }
 
     public Player player;
 
@@ -33,7 +47,7 @@ public class Achievement : MonoBehaviour
 
         if (CheckIfDone(totalBlocks, player.blocksPlaced) == true)
         {
-
+            ChangeVisibility(true, "500Blocks");
         }
 
         if (CheckIfDone(ScoreOver20times, player.scoreOver20Times) == true)
